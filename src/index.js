6a24b3cf1c7037255d6ba69f0633f809973444ac
Chekhuta Dmytro <chehuta1996@gmail.com>
2020-10-02 23:49:14 +0300

    feat: design min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0
+    }
 
-exports.min = function min (array) {
-  return 0;
+    let minNumber = array[0];
+    array.forEach((item) => {
+        minNumber = item < minNumber ? item : minNumber;
+    })
+
+    return minNumber;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0
+    }
+
+    let maxNumber = array[0];
+    array.forEach((item) => {
+        maxNumber = item > maxNumber ? item : maxNumber;
+    })
+    return maxNumber;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0
+    }
+
+    let sum = 0;
+    array.forEach((item) => {
+        sum += item;
+    })
+
+    return sum / array.length;
 }
